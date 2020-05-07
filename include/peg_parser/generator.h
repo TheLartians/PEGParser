637b50d548664e9ddc05b4bce347e7d3ad7db402
Lars Melchior <TheLartians@users.noreply.github.com>
2020-05-07 13:06:39 +0200

    remove arg name

--- include/peg_parser/generator.h
+++ include/peg_parser/generator.h
@@ -57,7 +57,7 @@ namespace peg_parser {
     template <class R2, typename... Args2> std::shared_ptr<peg::Rule> setProgramRule(
         const std::string &name, Program<R2, Args2...> subprogram,
         std::function<R(typename Interpreter<R2, Args2...>::Expression, Args...)> callback
-        = [](auto e, Args...) { return e.evaluate(); }) {
+        = [](auto e, Args &&...) { return e.evaluate(); }) {
       auto rule = getRule(name);
       rule->node = peg::GrammarNode::Rule(subprogram.parser.grammar);
       this->interpreter.setEvaluator(
