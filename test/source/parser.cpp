6d9ff944cbe56292b6e617aec909aa6273866a3b
Lars Melchior <TheLartians@users.noreply.github.com>
2020-05-07 14:16:10 +0200

    use explicit arguments

--- test/source/parser.cpp
+++ test/source/parser.cpp
@@ -148,7 +148,7 @@ TEST_CASE("Evaluation") {
     return res;
   });
   calculator.setProgramRule("Number", numberProgram,
-                            [](auto e, auto &&...) { return float(e.evaluate()); });
+                            [](Interpreter<int>::Expression e) { return float(e.evaluate()); });
   REQUIRE(calculator.run("42") == 42);
   REQUIRE(calculator.run("1+2") == 3);
   REQUIRE(calculator.run("2 * 3") == 6);
@@ -156,7 +156,7 @@ TEST_CASE("Evaluation") {
   REQUIRE(calculator.run("  1 + 2*3*1 +4 * 5  ") == 27);
   REQUIRE_THROWS(calculator.run("1+2*"));
 }
-#include <iostream>
+
 TEST_CASE("Left recursion") {
   ParserGenerator<float> calculator;
   calculator.setSeparatorRule("Whitespace", "[\t ]");
